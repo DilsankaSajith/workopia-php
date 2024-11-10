2415fa7485dbb83bffbd00b17991603f9db41bac
Sajith <Dilsanka39@gmail.com>
2024-11-10 23:43:43 +0530

    Created Router class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+require basePath('Router.php');
 
-require basePath('router.php');
+$router = new Router();
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$routes = require basePath('routes.php');
+
+$router->route($uri, $method);
