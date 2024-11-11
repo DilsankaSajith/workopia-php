6deb88c01b6331851b92e223581bf96410292d43
Dilsanka Sajith <Dilsanka39@gmail.com>
2024-11-11 15:02:25 +0530

    Before refactor database

--- Database.php
+++ Database.php
@@ -32,10 +32,16 @@ class Database
    * @return PDOStatement
    * @throws PDOExeption
    */
-  public function query($query)
+  public function query($query, $params = [])
   {
     try {
       $sth = $this->conn->prepare($query);
+
+      // Bind param names
+      foreach ($params as $param => $value) {
+        $sth->bindValue(':' . $param, $value);
+      }
+
       $sth->execute();
       return $sth;
     } catch (PDOException $e) {
